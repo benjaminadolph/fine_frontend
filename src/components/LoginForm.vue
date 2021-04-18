7c1d578787125698ced66a37927530d7c1aad440
Sarah Schneller <sarah@schneller-gestalten.de>
2021-04-18 17:21:22 +0200

    #frontend-login adds styling to frontend login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <form @submit.prevent="login">
+        <img class="fine-logo" src="@/assets/images/fineLogo.svg" />
+        <h1>Please sign in</h1>
+        <input v-model="email" type="email" placeholder="email" required>
+        <input v-model="password" type="password" placeholder="password" required>
+        <button type="submit">Sign in</button>
+    </form>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      email: 'Benjamin.Adolph@gmx.de',
+      password: '12345678',
+    };
+  },
+  methods: {
+    login() {
+      this.$store.dispatch('AUTH_REQUEST', {
+        email: this.email,
+        password: this.password,
+      })
+        .then(() => {
+          console.log('Logged In');
+          this.$router.push('/');
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,35 +1,14 @@
 <template>
-    <form @submit.prevent="login">
-        <h1>Please sign in</h1>
-        <input v-model="email" type="email" placeholder="email" required>
-        <input v-model="password" type="password" placeholder="password" required>
-        <button type="submit">Sign in</button>
-    </form>
+  <login-form />
 </template>
 
 <script>
+import LoginForm from '@/components/LoginForm.vue';
+
 export default {
   name: 'Login',
-  data() {
-    return {
-      email: 'Benjamin.Adolph@gmx.de',
-      password: '12345678',
-    };
-  },
-  methods: {
-    login() {
-      this.$store.dispatch('AUTH_REQUEST', {
-        email: this.email,
-        password: this.password,
-      })
-        .then(() => {
-          console.log('Logged In');
-          this.$router.push('/');
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
+  components: {
+    LoginForm,
   },
 };
 </script>
