e7eb771ab0ffee00ff6815239a556de737a074ad
jennici <jennifer.konopka@hotmail.de>
2021-04-19 23:28:47 +0200

    #33 added component Symptoms

---
+++ src/components/Symptoms.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="component">
+    <h1>Symptome</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Symptoms',
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+  @import "@/assets/scss/components/symptoms.scss";
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="view">
-    <h1>I am an example view</h1>
+    <h1>Dashboard</h1>
+    <h2>{{ today }}</h2>
+    <Symptoms />
   </div>
 </template>
 
@@ -9,12 +11,28 @@
 // IF YOU WANT TO USE A NEW VIEW YOU HAVE TO ADD IT TO
 // src/router/index.js AND THE ROUTER LINK TO src/App.vue
 
+import Symptoms from '@/components/Symptoms.vue';
+
 export default {
   name: 'Dashboard',
-  data() {
-    return {};
+  components: {
+    Symptoms,
   },
-  methods: {},
+  data() {
+    return {
+      heute: '',
+    };
+  },
+  methods: {
+    function() {
+      const heute = new Date();
+      const dd = String(heute.getDate()).padStart(2, '0');
+      const mm = String(heute.getMonth() + 1).padStart(2, '0');
+      const yyyy = heute.getFullYear();
+      this.heute = `${dd}.${mm}.${yyyy}`;
+      console.log(heute);
+    },
+  },
 };
 </script>
 
