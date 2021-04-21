2c7cc8ac5abf69489e1f86048cdd1c982600d87a
jennici <jennifer.konopka@hotmail.de>
2021-04-21 19:59:02 +0200

    #33 create Symptoms component

--- src/components/Symptoms.vue
+++ src/components/Symptoms.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="component">
-    <h1>Symptome</h1>
+    <header class="top">
+      <img src="@/assets/images/symptoms_icon_l.svg" />
+      <!-- add media queries to icons -->
+      <h2>Symptome</h2>
+    </header>
+    <div class="lastentry">
+      <header>
+        <p>Letzter Eintrag:</p>
+        <img src="@/assets/images/editButton_m.svg" />
+      </header>
+      <!-- needs to be an input with values -->
+      <p id="label">Migräne | Nacken | 21.02.2021 | 15:35</p>
+      <input type="range" min="0" max="6" name="intensity">
+    </div>
+    <AddButton />
   </div>
 </template>
 
 <script>
+import AddButton from '@/components/AddButton.vue';
 
 export default {
   name: 'Symptoms',
+  components: {
+    AddButton,
+  },
   data() {
     return {};
   },
