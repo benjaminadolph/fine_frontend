69229e4af70a0a3d0776dfafff4a210ff9799048
jennici <jennifer.konopka@hotmail.de>
2021-04-21 19:48:31 +0200

    #33 added Widget component to Dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="view">
     <h1>Dashboard</h1>
-    <h2>{{ today }}</h2>
+    <!-- <h2>{{ today }}</h2> -->
+    <Widget />
     <Symptoms />
   </div>
 </template>
@@ -11,26 +12,28 @@
 // IF YOU WANT TO USE A NEW VIEW YOU HAVE TO ADD IT TO
 // src/router/index.js AND THE ROUTER LINK TO src/App.vue
 
+import Widget from '@/components/Widget.vue';
 import Symptoms from '@/components/Symptoms.vue';
 
 export default {
   name: 'Dashboard',
   components: {
+    Widget,
     Symptoms,
   },
   data() {
     return {
-      heute: '',
+      today: '',
     };
   },
   methods: {
     function() {
-      const heute = new Date();
-      const dd = String(heute.getDate()).padStart(2, '0');
-      const mm = String(heute.getMonth() + 1).padStart(2, '0');
-      const yyyy = heute.getFullYear();
-      this.heute = `${dd}.${mm}.${yyyy}`;
-      console.log(heute);
+      const today = new Date();
+      const dd = String(today.getDate()).padStart(2, '0');
+      const mm = String(today.getMonth() + 1).padStart(2, '0');
+      const yyyy = today.getFullYear();
+      this.today = `${dd}.${mm}.${yyyy}`;
+      console.log(today);
     },
   },
 };
