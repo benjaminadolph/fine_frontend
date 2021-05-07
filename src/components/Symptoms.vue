084f31c016c79373c50147b5c711cd29e54c006f
Sarah Schneller <sarah@schneller-gestalten.de>
2021-05-08 01:51:27 +0200

    #130 adds circles by click on symptoms figure

--- src/components/Symptoms.vue
+++ src/components/Symptoms.vue
@@ -1,11 +1,14 @@
 <template>
-    <panZoom id="symptoms-figure-container" :options="{minZoom: 0.5, maxZoom: 5, center: true}" >
-       <IconComponent
-        v-bind:name="'woman-front'"
-        :size="653"
-        :width="227"
-        v-bind:color="primaryColor"
-      />
+    <panZoom
+      id="symptoms-figure-container"
+      v-on:click='addEntry($event)'
+      :options="{minZoom: 0.5, maxZoom: 5, center: true}" >
+        <IconComponent
+          v-bind:name="'woman-front'"
+          :size="653"
+          :width="227"
+          v-bind:color="primaryColor"
+        />
       <!-- <img src="@/assets/images/woman-front.svg" /> -->
     </panZoom>
 </template>
@@ -19,9 +22,27 @@ export default {
   components: {
     IconComponent,
   },
+  methods: {
+    addEntry(e) {
+      const { target } = e;
+      // Get the bounding rectangle of target
+      const rect = target.getBoundingClientRect();
+
+      // Mouse position
+      const x = Math.ceil(e.clientX - rect.left);
+      const y = Math.ceil(e.clientY - rect.top);
+      const container = e.target; // Get svg element
+      const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+      circle.setAttributeNS(null, 'cx', x);
+      circle.setAttributeNS(null, 'cy', y);
+      circle.setAttributeNS(null, 'r', 10);
+      circle.setAttributeNS(null, 'style', 'fill: currentColor;');
+      container.appendChild(circle);
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   @import "@/assets/scss/components/symptoms.scss";
 </style>
