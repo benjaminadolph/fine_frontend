4595da9f28f37975e18d873523c3cda00fe6f9b3
Sarah Schneller <sarah@schneller-gestalten.de>
2021-05-08 15:27:48 +0200

    #138 adds circle to set mouseclick

--- src/components/SymptomsEntry.vue
+++ src/components/SymptomsEntry.vue
@@ -1,18 +1,22 @@
 <template>
     <div>
       <SelectEntry module='symptoms' buttonLabel="Kategorie wählen" :multiselect=false />
-      <panZoom
+      <div
         id="symptoms-figure-container"
-        v-on:click='addEntry($event)'
-        :options="{minZoom: 0.5, maxZoom: 5, center: true}" >
+        v-touch:tap='addEntry'
+      >
+        <panZoom
+          id="figure"
+          :options="{maxZoom: 3}"
+        >
           <IconComponent
             v-bind:name="'woman-front'"
             :size="653"
-            :width="227"
+            :width="335"
             v-bind:color="primaryColor"
           />
-        <!-- <img src="@/assets/images/woman-front.svg" /> -->
-      </panZoom>
+        </panZoom>
+      </div>
       <router-link to="/module-entry/symptoms/33/details">
           <IconComponent name="plus-full" :size="50" />
       </router-link>
@@ -30,26 +34,41 @@ export default {
     IconComponent,
   },
   methods: {
-    addEntry(e) {
-      const { target } = e;
-      // Get the bounding rectangle of target
-      const rect = target.getBoundingClientRect();
+    addEntry(mouseEvent) {
+      const { target } = mouseEvent;
 
-      // Mouse position
-      const x = Math.ceil(e.clientX - rect.left);
-      const y = Math.ceil(e.clientY - rect.top);
-      const container = e.target; // Get svg element
-      const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-      circle.setAttributeNS(null, 'cx', x);
-      circle.setAttributeNS(null, 'cy', y);
-      circle.setAttributeNS(null, 'r', 10);
-      circle.setAttributeNS(null, 'style', 'fill: currentColor;');
-      container.appendChild(circle);
+      const figure = document.getElementById('653-woman-front');
+      if (figure === target) {
+        const figureRect = figure.getBoundingClientRect();
+
+        const relativeX = mouseEvent.clientX - figureRect.left;
+        const relativeY = mouseEvent.clientY - figureRect.top;
+        // Mouse position
+        const x = Math.ceil((663 / figureRect.height) * relativeX);
+        const y = Math.ceil((663 / figureRect.height) * relativeY);
+
+        const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        circle.setAttributeNS(null, 'cx', x);
+        circle.setAttributeNS(null, 'cy', y);
+        circle.setAttributeNS(null, 'r', 10);
+        circle.setAttributeNS(null, 'style', 'fill: currentColor;');
+        circle.setAttributeNS(null, 'onClick', 'console.log(\'hello\');');
+        circle.setAttributeNS(null, 'id', `circle-${x}-${y}`);
+        target.appendChild(circle);
+      }
+      
+
+      // const panContainer = document.getElementsByClassName('vue-pan-zoom-scene')[0];
+      /* const div = document.createElement('div');
+      div.classList.add('entry-circle');
+      div.style.top = `${y}px`;
+      div.style.left = `${x}px`;
+      figureContainer.appendChild(div); */
     },
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   @import "@/assets/scss/components/symptoms-entry.scss";
 </style>
