dd07753f7cb4e6dd609c279ead9790531bbe5473
Sarah Schneller <sarah@schneller-gestalten.de>
2021-05-22 16:50:09 +0200

    #13 adds calendar vue components

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,15 +1,18 @@
 <template>
-  <div>Calendar</div>
+  <div>
+    <CalendarMonth />
+  </div>
 </template>
 
 <script>
-
+import CalendarMonth from '@/components/CalendarMonth.vue';
 
 export default {
   name: 'Calendar',
   data() {
   },
   components: {
+    CalendarMonth,
   },
   methods: {
   },
