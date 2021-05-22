dc679a4de71ae933d3552de7ed90e41d0078503b
Sarah Schneller <sarah@schneller-gestalten.de>
2021-05-22 16:26:43 +0200

    removes vue calendar view

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,36 +1,17 @@
 <template>
-  <div>
-    <calendar-view
-      :show-date="showDate"
-      class="theme-default holiday-us-traditional holiday-us-official">
-      <template #header="{ headerProps }">
-        <calendar-view-header
-          :header-props="headerProps"
-          @input="setShowDate" />
-      </template>
-    </calendar-view>
-  </div>
+  <div>Calendar</div>
 </template>
 
 <script>
-import { CalendarView, CalendarViewHeader } from 'vue-simple-calendar';
-import 'vue-simple-calendar/dist/style.css';
+
 
 export default {
   name: 'Calendar',
   data() {
-    return {
-      showDate: new Date(),
-    };
   },
   components: {
-    CalendarView,
-    CalendarViewHeader,
   },
   methods: {
-    setShowDate(d) {
-      this.showDate = d;
-    },
   },
 };
 </script>
