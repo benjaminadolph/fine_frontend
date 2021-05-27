43e19a922d61dc249d6939efef9435113daf9c36
Sarah Schneller <sarah@schneller-gestalten.de>
2021-05-27 23:35:39 +0200

    #138 adds navigation to module entry

--- src/components/entry/ModuleEntry.vue
+++ src/components/entry/ModuleEntry.vue
@@ -1,8 +1,21 @@
 <template>
   <div
     class="module-entry-layer"
-    v-touch:swipe.left="swipeHandler"
-    v-touch:swipe.right="swipeHandlerRight">
+    v-touch:swipe.left="nextModule"
+    v-touch:swipe.right="prevModule">
+    <ul class="module-slider">
+      <li
+        v-for="_module in allModules"
+        :key="_module"
+        v-on:click="goToLayer(_module)">
+        <IconComponent
+          :name="_module"
+          :size="getSize(_module)"
+          :color="_module + '-primary'"
+          :class="{ 'not-active': isNotActive(_module)}"
+        />
+      </li>
+    </ul>
     <header class="fine-header">
       <div class="center">
           <h1 :class="currentModule + '-primary'">{{ getModuleName() }}</h1>
@@ -39,6 +52,7 @@ export default {
   data() {
     return {
       currentModule: this.module,
+      allModules: ['symptoms', 'emotions', 'nutrition'],
     };
   },
   methods: {
@@ -48,21 +62,45 @@ export default {
         moduleName = 'Symptome';
       } else if (this.currentModule === 'emotions') {
         moduleName = 'Gefühle';
+      } else if (this.currentModule === 'nutrition') {
+        moduleName = 'Ernährung';
       }
       return moduleName;
     },
-    swipeHandler() {
-      this.currentModule = 'emotions';
-    },
-    swipeHandlerRight() {
-      this.currentModule = 'symptoms';
-    },
-    getRouteBefore() {
-      console.log(this.$route.redirectedFrom);
-      return this.$route.redirectedFrom;
+    goToLayer(module) {
+      this.currentModule = module;
     },
     closeLayer() {
       this.$emit('closeLayer');
+    },
+    nextModule() {
+      let index = this.getModuleIndex(this.currentModule) + 1;
+      if (index >= this.allModules.length) {
+        index = 0;
+      }
+      this.currentModule = this.allModules[index];
+    },
+    prevModule() {
+      let index = this.getModuleIndex(this.currentModule) - 1;
+      if (index < 0) {
+        index = this.allModules.length - 1;
+      }
+      this.currentModule = this.allModules[index];
+    },
+    getModuleIndex(module) {
+      return this.allModules.indexOf(module);
+    },
+    isNotActive(module) {
+      if (module !== this.currentModule) {
+        return true;
+      }
+      return false;
+    },
+    getSize(module) {
+      if (module !== this.currentModule) {
+        return 24;
+      }
+      return 32;
     },
   },
 };
