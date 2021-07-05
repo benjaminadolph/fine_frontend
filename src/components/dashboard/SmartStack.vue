d4dfa39548fb135bf607b18e45a32457bd884ce0
jennici <jennifer.konopka@hotmail.de>
2021-07-05 21:37:34 +0200

    get first name

--- src/components/dashboard/SmartStack.vue
+++ src/components/dashboard/SmartStack.vue
@@ -2,20 +2,35 @@
   <div class="component">
         <img src="@/assets/images/fine_smile.svg" />
         <header>
-          <p class="plain-m-bold">Hallo Eva!</p>
+          <p class="plain-m-bold">Hallo {{ firstName }}</p>
           <p class="plain-m-book">Hier könnte eine Nachricht stehen</p>
         </header>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 
 export default {
   name: 'SmartStack',
   data() {
-    return {};
+    return {
+      profile: {},
+      firstName: '',
+    };
   },
-  methods: {},
+  computed: {
+    ...mapGetters(['getUserProfile']),
+  },
+  mounted() {
+    this.getProfile();
+  },
+  methods: {
+    getProfile() {
+      this.profile = this.getUserProfile;
+      this.firstName = this.profile.firstName;
+    },
+  },
 };
 </script>
 
