a51c4a56e90a002492cf8d1622adf4bde461567d
jennici <jennifer.konopka@hotmail.de>
2021-07-05 21:37:00 +0200

    reduce code to one prop and return right data

--- src/components/dashboard/LastEntry.vue
+++ src/components/dashboard/LastEntry.vue
@@ -3,98 +3,56 @@
     <header>
       <p class="plain-m-book">Letzter Eintrag:</p>
       <img src="@/assets/icons/16-pencil.svg" />
+      <!-- was passiert beim Hovern über den Button? -->
     </header>
-    <p class="plain-m-bold" id="label" v-if="(module === 'symptoms')">
-      {{ symptoms.category + ' |' }}
-      {{ symptoms.location.title + ' |' }}
-      {{ getDate + ' |' }}
-      {{ getLastEntryTime(module) + ' Uhr' }}
-    </p>
-    <p class="plain-m-bold" id="label" v-if="(module === 'emotions')">
-      {{ emotions.title + ' |' }}
-      {{ emotions.date + ' |' }}
-      {{ getLastEntryTime(module) + ' Uhr' }}
-    </p>
+    <div class="label" v-if="defaultText != null">
+      <p class="plain-m-bold">
+      {{ getTitle() + ' | ' }}
+      </p>
+      <p class="plain-m-bold">
+       {{ getDate() + ' | ' }}
+      </p>
+      <p class="plain-m-bold">
+       {{ getTime() + ' Uhr' }}
+      </p>
+    </div>
+    <div class="label" v-if="defaultText == null">
+      <p class="plain-m-bold">
+        {{ defaultText }}
+      </p>
+    </div>
     <input type="range" min="0" max="6" name="intensity" />
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import {
-  GET_SYMPTOM,
-} from '@/store/modules/symptoms';
-import {
-  GET_EMOTION,
-} from '@/store/modules/emotions';
+import dayjs from 'dayjs';
 
 export default {
   name: 'LastEntry',
   props: {
     module: String,
-  },
-  data() {
-    return {
-      symptoms: [],
-      emotions: [],
-    };
-  },
-  computed: {
-    ...mapGetters(['getUserProfile', 'getLatestSymptom', 'getUserEmotions']),
-  },
-  mounted() {
-    this.getSymptom();
-    this.getEmotion();
+    lastEntry: Object,
+    defaultText: String,
   },
   methods: {
-    getSymptom(id) {
-      this.$store.dispatch(GET_SYMPTOM, {
-        symptom_id: id,
-      })
-        .then(() => {
-          this.symptoms = this.getLatestSymptom;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-    getEmotion(id) {
-      this.$store.dispatch(GET_EMOTION, {
-        emotion_id: id,
-      })
-        .then(() => {
-          this.emotions = this.getUserEmotions;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
     getDate() {
-      const dd = String(this.symptoms.date.getDate()).padStart(2, '0');
-      const mm = String(this.symptoms.date.getMonth() + 1).padStart(2, '0');
-      const yyyy = this.symptoms.date.getFullYear();
-      date = `${dd}.${mm}.${yyyy}`;
-
-      return date;
+      return dayjs(this.lastEntry.date).format('DD.MM.YYYY');
     },
-    /* getLastEntryDate(module) {
-      let lastEntryDate = '';
-      if (module === 'symptoms') {
-        lastEntryDate = '19.05.2020';
-      } else if (module === 'emotions') {
-        lastEntryDate = '28.04.2021';
-      }
-      return lastEntryDate;
-    }, */
-    getLastEntryTime(module) {
-      let lastEntryTime = '';
-      if (module === 'symptoms') {
-        lastEntryTime = '19:15';
-      } else if (module === 'emotions') {
-        lastEntryTime = '15:45';
-      }
-      return lastEntryTime;
+    getTime() {
+      return dayjs(this.lastEntry.date).format('HH:mm');
     },
+    getTitle() {
+      let title = '';
+      console.log(this.lastEntry);
+      if (this.module === 'symptoms') {
+        title = `${this.lastEntry.category}`;
+      } else if (this.module === 'emotions') {
+        // title = this.lastEntry.emotion.join(' | ');
+      }
+      return title;
+    },
+    //  | ${this.lastEntry.location.title}
   },
 };
 </script>
